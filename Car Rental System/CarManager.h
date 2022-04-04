50e8f7ba1ef773271216bd9c4f19b1b7c201ac1c
Elsonnnn <90232363+Elsonnnn@users.noreply.github.com>
2022-04-05 01:38:56 +0800

    Update CarManager.h

--- Car Rental System/CarManager.h
+++ Car Rental System/CarManager.h
@@ -6,9 +6,14 @@
 class CarManager
 {
 private:
+	static CarManager* s_instance;
 	std::vector<Car> cars;
+	CarManager(){};
 public:
-	CarManager();
+	static CarManager* getInstance(){
+		if(!s_instance)
+			s_instance = new CarManager;
+		return s_instance;
 	std::vector<Car> getCars();
 	void addCar(Car);
 	void removeCar(Car);
